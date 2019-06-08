3f0e056f5aa776c0ee1f3bb12e49d642f5a53c71
AlexandreGabrielli <alexandre.gabrielli@users.noreply.github.com>
2019-06-08 14:42:31 +0200

    some nice

--- Snake/src/main/java/SnakeManager.java
+++ Snake/src/main/java/SnakeManager.java
@@ -1,11 +1,29 @@
+import handler.SnakeHandler.SnakeSegment;
+import jdk.internal.net.http.common.Pair;
+import lombok.Getter;
+import lombok.Setter;
+
+
+import java.util.List;
+
 /**
  * 
  */
 public class SnakeManager {
+    public class Coordinate{
+        @Getter @Setter private int x;
+        @Getter @Setter  private int y;
+
+        public Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
     /**
      *
      */
-    public Board  board;
+    private List<Pair<SnakeSegment,Coordinate>> snake;
+    private List<Pair< Edible,Coordinate>> edibles;
 
     /**
      * Default constructor
