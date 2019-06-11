a7ddc1c886a5413ae1bb3aaa39893024aea1f083
alexandre.gabrielli <alexandre.gabrielli@users.noreply.github.com>
2019-06-11 22:08:13 +0200

    color add implementation

--- Snake/src/main/java/request/SimpleColorRequest.java
+++ Snake/src/main/java/request/SimpleColorRequest.java
@@ -5,15 +5,20 @@ import java.awt.*;
 public class SimpleColorRequest implements Request {
 
     private Color color;
+    private int score;
 
-    public SimpleColorRequest(Color color) {
+    public SimpleColorRequest(Color color, int score) {
         this.color = color;
+        this.score = score;
     }
 
     public Color getColor() {
         return color;
     }
 
+    public int getScore() {
+        return score;
+    }
 
     @Override
     public RequestType getRequestType() {
